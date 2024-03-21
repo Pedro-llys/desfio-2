df10dba0c79d775fe2851c613a391314e6cbb8b2
Jefferson <bestfx@hotmail.com>
2024-03-20 21:48:57 -0300

    estudando operadores logicos

--- index.js
+++ index.js
@@ -43,8 +43,74 @@ function saleStatus(status, total){
 saleStatus('approved', 200)
 
 function discountCalc(price, discount){
-    let final = total -total*(price*discount/200);
+    let final = price -price*(price-discount/200);
     return final
 }
 
-console.log('O valor com deconto é: R$' + valueDiscount(30, 200))
+console.log('O valor com deconto é: R$' + discountCalc(30, 200))
+
+
+
+
+
+let num1 = 1;
+let num2 = 2;
+
+console.log(num1 + num2)
+console.log(num1 - num2)
+console.log(num1 * num2)
+console.log(num1 % num2)
+console.log(num1 ** num2)
+console.log(Math.sqrt(25))
+
+//Operadores de atibuição
+
+let num3 = 10;
+console.log(num3 +=20);
+console.log(num3 -=20);
+console.log(num3 *=20);
+console.log(num3 /=20);
+
+//Operadores de comparação 
+
+let num4 = 10
+
+console.log(num4>0)
+console.log(num4<0)
+console.log(num4>=10)
+console.log(num4<=10)
+console.log(num4 > num1)
+
+console.log(num4 !=10)
+console.log(num4 == 10)
+
+//Operadores de igualdade
+
+let num5 = 1
+let num6 = '1'
+console.log(num5 == num6);
+console.log(num5 === num6);
+
+//Operadores Ternários
+
+let driver= 120;
+let speed = driver>120? 'Above':'Below';
+console.log(speed);
+
+//Operadores Lógicos com Strings
+let temIdadeMinima = false;
+let temTituloEleitor = true;
+
+let podeVotar = temIdadeMinima && temTituloEleitor;
+console.log(podeVotar);
+
+let podeViajar = !podeVotar;
+console.log(podeViajar);
+
+//Operadores Logicos com strings 
+
+let corCliente = undefined
+let corEstoque = 'Black'
+let corVendida = corCliente || corEstoque
+
+console.log(corVendida)
